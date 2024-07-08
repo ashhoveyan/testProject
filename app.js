bf6f23244d1c272be91c0a0e379a2ee2d4124c68
Misha Ghalachyan <mish.ghalachyan@gmail.com>
2024-07-08 17:29:35 +0400

    hellooo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ dotenv.config();
 console.log(process.env.PORT);
 console.log(process.env.PORT);
 console.log(123)
-console.log(123)
+console.log(fantastic)
 console.log(123)
 console.log(123)
 console.log(123)
